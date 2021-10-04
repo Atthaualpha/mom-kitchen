2281f7a236d8fb9fa9787e82851a434e5172f767
Sebastian Céspedes Osorio <scespedes@conexia.com>
2021-10-03 22:13:08 -0500

    change dto

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -1,4 +1,5 @@
 import { CreateItemDto } from '../dto/request/createItemDto';
+import { ModifyItemDto } from '../dto/request/modifyItemDto';
 import {
   Body,
   Controller,
@@ -15,7 +16,7 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Request, Response } from 'express';
 import { ItemService } from '../services/item.service';
-import { UpdateItemDto } from '../dto/request/updateItemDto';
+
 
 @Controller('item')
 export class ItemController {
@@ -51,7 +52,7 @@ export class ItemController {
   }
 
   @Put()
-  updateItem(@Body() req: UpdateItemDto, @Res() res: Response) {
+  updateItem(@Body() req: ModifyItemDto, @Res() res: Response) {
     this.itemService.updateItem(req, (resp: any, error: any) => {
       if (error) {
         res.status(500).send(error);
