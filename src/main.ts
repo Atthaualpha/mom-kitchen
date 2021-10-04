d25dc7141210817ee1da7018f2b8dd92d1109a86
Sebastian Céspedes Osorio <scespedes@conexia.com>
2021-10-03 22:22:19 -0500

    add port

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors()
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
     module.hot.accept();
